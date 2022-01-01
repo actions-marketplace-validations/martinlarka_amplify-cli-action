0e290f30f125d20eb76404d66ebc17c53d0a1139
Fredo Martinez <fredomartinezm@gmail.com>
2022-01-01 15:21:53 +0100

    First commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:14.18.2-alpine3.14
+
+RUN npm config set user 0 && \
+  npm config set unsafe-perm true && \
+  npm install -g @aws-amplify/cli@7.6.5
+
+COPY entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -l
+
+set -e
+
+if [ -z "$AWS_ACCESS_KEY_ID" ] && [ -z "$AWS_SECRET_ACCESS_KEY" ] ; then
+  echo "You must provide the action with both AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables in order to deploy"
+  exit 1
+fi
+
+env=$1
+
+aws_config_file_path="$(pwd)/aws_config_file_path.json"
+echo '{"accessKeyId":"'$AWS_ACCESS_KEY_ID'","secretAccessKey":"'$AWS_SECRET_ACCESS_KEY'","region":"'$AWS_REGION'"}' > $aws_config_file_path
+echo '{"projectPath": "'"$(pwd)"'","defaultEditor":"code","envName":"'$env'"}' > ./amplify/.config/local-env-info.json
+echo '{"'$env'":{"configLevel":"project","useProfile":false,"awsConfigFilePath":"'$aws_config_file_path'"}}' > ./amplify/.config/local-aws-info.json
+
+amplify env checkout $env && amplify status
