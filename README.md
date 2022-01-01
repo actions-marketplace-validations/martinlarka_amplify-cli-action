4212eb55b9aca04d8b1dcf6826136626a3984720
Fredo Martinez <fredomartinezm@gmail.com>
2022-01-01 19:45:26 +0100

    Update action

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# amplify-cli-action
+Amplify CLI Deploy Action
+
+## Inputs
+
+## `env`
+
+**Required** The name of the environment. Default `"dev"`.
+
+## Example usage
+
+uses: actions/checkout@v2
+name: Amplify CLI Deploy Action
+  uses: cordobeces/amplify-cli-action@v1.0.0
+  with:
+    env: dev
+  env:
+    AWS_ACCESS_KEY_ID: ${{ secrets.AWS_ACCESS_KEY_ID }}
+    AWS_SECRET_ACCESS_KEY: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+    AWS_REGION: us-east-1
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,10 @@ if [ -z "$AWS_ACCESS_KEY_ID" ] && [ -z "$AWS_SECRET_ACCESS_KEY" ] ; then
 fi
 
 env=$1
-echo $env
 
 aws_config_file_path="$(pwd)/aws_config_file_path.json"
 echo '{"accessKeyId":"'$AWS_ACCESS_KEY_ID'","secretAccessKey":"'$AWS_SECRET_ACCESS_KEY'","region":"'$AWS_REGION'"}' > $aws_config_file_path
 echo '{"projectPath": "'"$(pwd)"'","defaultEditor":"code","envName":"'$env'"}' > ./amplify/.config/local-env-info.json
 echo '{"'$env'":{"configLevel":"project","useProfile":false,"awsConfigFilePath":"'$aws_config_file_path'"}}' > ./amplify/.config/local-aws-info.json
 
-amplify env checkout $env && amplify status
+amplify env checkout $env && amplify publish
